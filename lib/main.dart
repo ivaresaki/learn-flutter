ca1e63978c7d5963b504019c94e9e97772de7fa9
ivaresaki <ivaresaki@gmail.com>
2024-02-02 15:25:50 -0500

    refactor main with Home widget. Add image in assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,41 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    title: 'Flutter App',
-  ));
+void main() => runApp(
+      const MaterialApp(
+        home: Home(),
+      ),
+    );
+
+void onFloatingActionButtonPress() {
+  if (kDebugMode) {
+    print('button pressed');
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('My App'),
+        backgroundColor: Colors.red[600],
+      ),
+      body: const Center(
+        child: Image(
+          image: AssetImage('assets/space1.avif'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        onPressed: () {
+          onFloatingActionButtonPress();
+        },
+        child: const Text('click'),
+      ),
+    );
+  }
 }
